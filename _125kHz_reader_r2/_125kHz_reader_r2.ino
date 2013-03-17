b04538d3c5ae1247fdd401d2d334689b8d8715b8
goliat90 <goliat.90@gmail.com>
2013-03-17 17:10:40 +0100

    Viktor är så envis med pekare! :D

--- _125kHz_reader_r2/_125kHz_reader_r2.ino
+++ _125kHz_reader_r2/_125kHz_reader_r2.ino
@@ -4,28 +4,34 @@ void setup(){
 }
 
 //function to read tag, we recieve the array we write the info to
-int* readTag(){
-  //array where we add tag data.
-  int tag[14];
-  
+void readTag(byte* pt){
   //Delay for data to arrive into the serial buffer.
-   delay(200);
-    //Transfering data from serial to the tag in the loop
-    for(int i = 0; i < 14; i++){
-      // read the incoming number on serial RX
-      tag[i] = Serial.read();
-      //transfering it to the array.
-    
+  delay(200);
+
+  //Transfering data from serial to the tag in the loop
+  for(int i = 0; i < 14; i++){
+    // read the incoming number on serial RX
+    *pt = Serial.read();
+    pt++;
   }
   //we flush the serial so we dont have residual data when we read it again.
   Serial.flush();
-  //Return the tag array.
-  return tag;
 }
 
-void printTag(int* inputTag ){
-  int* tag = inputTag;
-  Serial.println("New Tag");
+void printTag(){
+  
+}
+
+void loop() 
+{
+  byte tag[14];
+
+  if(Serial.available() > 0) {  
+    //we call on the read function and save the array to a pointer.
+    readTag(tag);
+    
+    
+    Serial.println("New Tag");
     //printing data
     for(int i = 1; i < 11; i++){
       //Check if it's a number or letter
@@ -37,30 +43,10 @@ void printTag(int* inputTag ){
         Serial.print((tag[i]-55), HEX);
         Serial.print(" ");
       }
-
     }
-    Serial.println("\nEnd of Tag");
-}
+    Serial.println("");
+    Serial.println("End of Tag");
 
-boolean crcCheck(int* inputTag) {
-  int* tag = inputTag;
-  int checkValue1 = 0;
-  int checkValue2 = 0;
-  for(int i=1;i<11; i+=2) {
-    checkValue1 = checkValue1 ^ tag[i];
-  }
-  for(int i=2;i<11; i+=2) {
-    checkValue2 = checkValue2 ^ tag[i];
-  }
-  return (checkValue1 == tag[11])&&(checkValue2 == tag[12]);
-}
-
-void loop() 
-{
-  if(Serial.available()){
-    int* tag = readTag();
-    //if(crcCheck(tag)){
-    printTag(tag);
   }
 }
 
